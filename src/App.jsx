29fb2abfef0055b4ad4ff4dc085c684c3faae324
Debby-arch <opiyorosedebrah@gmail.com>
2026-01-25 04:01:38 +0300

    Services and title files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Navbar/Hero/Hero'
+import Services from './Components/Navbar/Services/Services'
 
 
 const App = () => {
@@ -8,6 +9,8 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero />
+      <div className="container"></div>
+        <Services/>
     </div>
   )
 }
--- src/Components/Navbar/Services/Services.jsx
+++ src/Components/Navbar/Services/Services.jsx
@@ -10,15 +10,31 @@ const Services = () => {
     <div className='services'>
         <div className='service'>
             <img src={digitization} alt=""/>
+            <div className="caption">
+                <img src="" alt=""/>
+                <p>School and SME Digitisation</p>
+            </div>
         </div>
         <div className='service'>
             <img src={automation} alt=""/>
+            <div className="caption">
+                <img src="" alt=""/>
+                <p>Automation and Operation Support for Founders</p>
+            </div>
         </div>
         <div className='service'>
             <img src={excel} alt=""/>
+            <div className="caption">
+                <img src="" alt=""/>
+                <p>Excel, Data and Digital Skills Training</p>
+            </div>
         </div>
         <div className='service'>
             <img src={design} alt=""/>
+            <div className="caption">
+                <img src="" alt=""/>
+                <p>UI/UX & MVP Design for Early-Stage Startups</p>
+            </div>
         </div>
     </div>
   )
